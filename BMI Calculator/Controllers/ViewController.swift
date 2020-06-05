ed679f0185a2748a625b94bcbee5daf2f8830615
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-06-05 14:36:10 +0200

    IBAction sliders - rounded value, connection

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var heightLabel: UILabel!
+  @IBOutlet weak var weightLabel: UILabel!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  @IBAction func heightSliderChanged(_ sender: UISlider) {
+    print(String(format: "%.2f", sender.value)) // 2f -> 2 decimal places; (round(sender.value*100)/100)
+    
+    let height = String(format: "%.2f", sender.value)
+    heightLabel.text = "\(height) m" // String
+  }
+  
+  @IBAction func weightSliderChanged(_ sender: UISlider) {
+    print(Int(ceil(sender.value))) // to whole number 101.440926 -> 102, Int(ceil(sender.value)
+    
+    let weight = String(format: "%.0f", sender.value)
+    weightLabel.text = "\(weight) Kg" // String
+    
+  }
+  
 }
 
